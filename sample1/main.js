9bb55b490d35d4b9f37c960c31e206535cb97433
almazhou <zx_new@126.com>
2014-10-10 16:56:18 +0800

    directive of attribute

--- sample1/main.js
+++ sample1/main.js
@@ -5,4 +5,13 @@ app.directive("superman",function() {
 		restrict: "E",
 		template: "<div> Here I am to save the day </div>"
 	}
-})
+});
+
+app.directive("superattribute",function() {
+	return {
+		restrict: "A",
+		link: function() {
+            alert("I'm working stronger")
+        }
+	}
+});
